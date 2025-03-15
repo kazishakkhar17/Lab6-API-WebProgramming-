e8ea74b71e7b09192b98de7eed9caa5c5856fdc4
kazishakkhar17 <kazishakkhar04@gmail.com>
2025-03-15 10:25:30 +0600

    Added PUT and DELETE routes for updating and deleting users

--- Backend/index.js
+++ Backend/index.js
@@ -37,6 +37,52 @@ app.post('/api/users', async (req, res) => {
   }
 });
 
+// PUT route to update a user
+app.put('/api/users/:idStudent', async (req, res) => {
+  const { idStudent } = req.params; 
+  const { name, email, password, age, cgpa, department } = req.body;
+
+  try {
+    // Find the user by idStudent and update
+    const updatedUser = await User.findOneAndUpdate(
+      { idStudent }, // Use idStudent to search
+      { name, email, password, age, cgpa, department }, // Fields to update
+      { new: true } // Return updated document
+    );
+
+    if (!updatedUser) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.status(200).send(updatedUser);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while updating the user."
+    });
+  }
+});
+
+// DELETE route to delete a user
+app.delete('/api/users/:idStudent', async (req, res) => {
+  const { idStudent } = req.params; // Get idStudent from the URL parameter
+
+  try {
+    const deletedUser = await User.findOneAndDelete({ idStudent }); // Find and delete by idStudent
+
+    if (!deletedUser) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.status(200).send({ message: "User deleted successfully" });
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while deleting the user."
+    });
+  }
+});
+
+
+
 mongoose.connect("mongodb+srv://kazishakkhar04:ZkoZBplIfAJvIo40@cluster0.khwct.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log('Connected to MongoDB!');
