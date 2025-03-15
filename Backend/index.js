a9808b679eab83dd28fb8e6542c911812503f980
kazishakkhar17 <kazishakkhar04@gmail.com>
2025-03-15 10:19:26 +0600

    made post request

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,8 @@ const User = require('./models/user');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -16,6 +18,22 @@ app.get('/api/users', async (req, res) => {
     res.status(500).send({
       message: err.message || "Some error occurred while retrieving users."
     });
+  }
+});
+// POST route to add a new user
+app.post('/api/users', async (req, res) => {
+  const { idStudent, name, email, password, age, cgpa, department } = req.body;
+
+  if (!idStudent || !department) {
+    return res.status(400).send({ message: "idStudent and department are required" });
+  }
+
+  try {
+    const newUser = new User({ idStudent, name, email, password, age, cgpa, department });
+    const savedUser = await newUser.save();
+    res.status(201).send(savedUser);
+  } catch (err) {
+    res.status(500).send({ message: err.message || "Some error occurred while adding the user." });
   }
 });
 
